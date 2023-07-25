1a032ebfc368b1761e366c1906d3f3c59ddb25f8
dimchee <dimchee90@gmail.com>
2023-07-25 21:13:31 +0200

    Add Functionality

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,117 @@
 module Main exposing (..)
 
 import Browser
-import View
+import Dict exposing (Dict)
+import Html
+import Html.Attributes
+import View exposing (Entity, Msg(..), Tag)
 
 
-main : Program () () ()
+type alias Rel =
+    { tags : Dict Entity (List Tag)
+    , ents : Dict Tag (List Entity)
+    }
+
+
+type View
+    = Main
+    | Tag Tag
+    | Entity Entity -- | Graph
+
+
+type alias Model =
+    { rel : Rel
+    , view : View
+    }
+
+
+viewMain : Rel -> Html.Html Msg
+viewMain { tags } =
+    View.body
+        [ View.search "search..."
+        , Html.div [] <| List.map (\( ent, ts ) -> View.container ent ts) <| Dict.toList tags
+        , View.addEntity
+        ]
+
+
+viewTag : Tag -> Rel -> Html.Html Msg
+viewTag tag { ents } =
+    View.body
+        [ View.backToMain
+        , View.tag tag
+        , Dict.get tag ents
+            |> Maybe.withDefault []
+            |> List.map View.entity
+            |> Html.div [ Html.Attributes.style "display" "flex" ]
+        ]
+
+
+viewEntity : Entity -> Rel -> Html.Html Msg
+viewEntity ent { tags } =
+    View.body
+        [ View.backToMain
+        , View.entity ent
+        , Dict.get ent tags
+            |> Maybe.withDefault []
+            |> List.map View.tag
+            |> Html.div [ Html.Attributes.style "display" "flex" ]
+        ]
+
+
+
+-- main : Program () Model String
+
+
+testRel : Rel
+testRel =
+    { tags =
+        Dict.fromList
+            [ ( "ent1", [ "tag1", "tag2", "tag3" ] )
+            , ( "ent2", [ "tag1", "tag4", "tag2" ] )
+            ]
+    , ents =
+        Dict.fromList
+            [ ( "tag1", [ "ent1", "ent2" ] )
+            , ( "tag2", [ "ent1", "ent2" ] )
+            , ( "tag3", [ "ent1" ] )
+            , ( "tag4", [ "ent2" ] )
+            ]
+    }
+
+
+main : Program () Model Msg
 main =
     Browser.sandbox
-        { init = ()
+        { init = { rel = testRel, view = Main }
         , view =
-            \_ ->
-                View.body
-                    [ View.container (View.entity "entity1") [ View.tag "tag1", View.new ]
-                    , View.search "search..."
-                    , View.add
-                    ]
-        , update = \_ _ -> ()
+            \{ rel, view } ->
+                case view of
+                    Main ->
+                        viewMain rel
+
+                    Tag tag ->
+                        viewTag tag rel
+
+                    Entity entity ->
+                        viewEntity entity rel
+        , update = update
         }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SelectedEntity ent ->
+            { model | view = Entity ent }
+
+        SelectedTag tag ->
+            { model | view = Tag tag }
+
+        BackToMain ->
+            { model | view = Main }
+
+        AddTag ent ->
+            model
+
+        AddEntity ->
+            model
--- src/View.elm
+++ src/View.elm
@@ -2,6 +2,7 @@ module View exposing (..)
 
 import Html exposing (Html)
 import Html.Attributes
+import Html.Events
 
 
 font_size : String
@@ -9,12 +10,28 @@ font_size =
     "1.5em"
 
 
+type alias Entity =
+    String
+
+
+type alias Tag =
+    String
+
+
+type Msg
+    = SelectedEntity Entity
+    | SelectedTag Tag
+    | AddTag Entity
+    | AddEntity
+    | BackToMain
+
+
 marginated : String -> Html msg
 marginated text =
     Html.div [ Html.Attributes.style "margin" "10px" ] [ Html.text text ]
 
 
-tag : String -> Html msg
+tag : String -> Html Msg
 tag text =
     Html.div
         [ Html.Attributes.style "display" "inline-block"
@@ -22,11 +39,12 @@ tag text =
         , Html.Attributes.style "text-align" "center"
         , Html.Attributes.style "outline" "2px solid white"
         , Html.Attributes.style "margin" "10px"
+        , Html.Events.onClick <| SelectedTag text
         ]
         [ marginated text ]
 
 
-entity : String -> Html msg
+entity : String -> Html Msg
 entity text =
     Html.div
         [ Html.Attributes.style "display" "inline-block"
@@ -34,6 +52,7 @@ entity text =
         , Html.Attributes.style "text-align" "center"
         , Html.Attributes.style "outline" "2px solid white"
         , Html.Attributes.style "margin" "10px"
+        , Html.Events.onClick <| SelectedEntity text
         ]
         [ marginated text ]
 
@@ -41,17 +60,21 @@ entity text =
 search : String -> Html msg
 search text =
     Html.div
-        [ Html.Attributes.style "display" "inline-block"
-        , Html.Attributes.style "border-radius" "0.5em"
-        , Html.Attributes.style "text-align" "center"
-        , Html.Attributes.style "outline" "2px dashed white"
-        , Html.Attributes.style "margin" "10px"
-        , Html.Attributes.style "color" "#888"
+        [ Html.Attributes.style "display" "flex"
+        , Html.Attributes.style "justify-content" "center"
         ]
-        [ marginated <| "🔍 " ++ text ]
+        [ Html.div
+            [ Html.Attributes.style "width" "60%"
+            , Html.Attributes.style "border-radius" "0.5em"
+            , Html.Attributes.style "outline" "2px dashed white"
+            , Html.Attributes.style "margin" "10px"
+            , Html.Attributes.style "color" "#888"
+            ]
+            [ marginated <| "🔍 " ++ text ]
+        ]
 
 
-container : Html msg -> List (Html msg) -> Html msg
+container : Entity -> List Tag -> Html Msg
 container ent tags =
     Html.div
         [ Html.Attributes.style "display" "flex"
@@ -60,42 +83,47 @@ container ent tags =
         , Html.Attributes.style "outline" "2px solid white"
         , Html.Attributes.style "margin" "10px"
         ]
-        [ ent
+        [ entity ent
         , Html.div
             [ Html.Attributes.style "display" "flex"
             , Html.Attributes.style "background" "linear-gradient(#F00, #F00) no-repeat left/2px 100%"
             ]
-            tags
+          <|
+            List.map tag tags
+        , addTag ent
         ]
 
 
-new : Html msg
-new =
+addTag : Entity -> Html Msg
+addTag ent =
     Html.div
         [ Html.Attributes.style "background" "linear-gradient(#888, #888) no-repeat left/2px 60%"
-        , Html.Attributes.style "padding" "1% 2% 1% 2%"
+        , Html.Attributes.style "padding" "1%"
         , Html.Attributes.style "text-align" "center"
         , Html.Attributes.style "margin" "10px"
         , Html.Attributes.style "color" "#888"
         ]
-        [ marginated "new..." ]
+        [ Html.div [ Html.Events.onClick <| AddTag ent ] [ Html.text "new..." ] ]
 
 
 body : List (Html msg) -> Html msg
 body =
-    Html.div
-        [ Html.Attributes.style "background-color" "#111"
-        , Html.Attributes.style "color" "white"
-        , Html.Attributes.style "margin" "0"
-        , Html.Attributes.style "position" "fixed"
-        , Html.Attributes.style "height" "100vh"
-        , Html.Attributes.style "width" "100vw"
-        , Html.Attributes.style "font-size" font_size
-        ]
+    Html.div [ Html.Attributes.style "margin" "10%" ]
+        >> List.singleton
+        >> Html.div
+            [ Html.Attributes.style "background-color" "#111"
+            , Html.Attributes.style "color" "white"
+            , Html.Attributes.style "margin" "0"
+            , Html.Attributes.style "position" "fixed"
+            , Html.Attributes.style "height" "100vh"
+            , Html.Attributes.style "width" "100vw"
+            , Html.Attributes.style "font-size" font_size
+            , Html.Attributes.style "user-select" "none"
+            ]
 
 
-add : Html msg
-add =
+addEntity : Html Msg
+addEntity =
     Html.div
         [ Html.Attributes.style "display" "flex"
         , Html.Attributes.style "align-items" "center"
@@ -104,10 +132,31 @@ add =
         [ Html.div
             [ Html.Attributes.style "margin" "10px"
             , Html.Attributes.style "font-size" "2em"
+            , Html.Events.onClick <| AddEntity
             ]
             [ Html.text "+" ]
         , Html.div
             [ Html.Attributes.style "background" "linear-gradient(#888, #888) no-repeat left/2px 60%"
             ]
             [ marginated "add entity..." ]
+        ]
+
+
+backToMain : Html.Html Msg
+backToMain =
+    Html.div
+        [ Html.Attributes.style "display" "flex"
+        , Html.Attributes.style "align-items" "center"
+        , Html.Attributes.style "color" "#888"
+        ]
+        [ Html.div
+            [ Html.Attributes.style "margin" "10px"
+            , Html.Attributes.style "font-size" "2em"
+            , Html.Events.onClick <| BackToMain
+            ]
+            [ Html.text "⌂" ]
+        , Html.div
+            [ Html.Attributes.style "background" "linear-gradient(#888, #888) no-repeat left/2px 60%"
+            ]
+            [ marginated "Home" ]
         ]
